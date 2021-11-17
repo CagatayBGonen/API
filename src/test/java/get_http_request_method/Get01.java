ccc8d112b194c6b6ab84bc1794a7057b452b641a
Cagatay <cagataybayramgonen@gmail.com>
2021-11-17 07:20:57 +0300

    get01

--- src/test/java/get_http_request_method/Get01.java
+++ src/test/java/get_http_request_method/Get01.java
@@ -40,6 +40,24 @@ public class Get01 {
         response.prettyPrint(); // for seeing response on consol
 
         //4)Do assertion
+        /*
+            If you have multiply errors on Assertion, execution will be stopped in the first error. Next code will not be executed.
+            You will not get any error messages about the second, thirs etc. error.
+            That kind of assertions are called "Hard Assertions"
+            Do we have soft assertions? Yes.
+            In Soft Assertion(Verification), all codes will be executed and you will get report for all assertions.
+        */
+        response.then().assertThat().statusCode(200).contentType("application/json").statusLine("HTTP/1.1 200 OK");
+
+        //How to print status code, status line, content type, etc. on the console
+        System.out.println("Status Code is "+response.getStatusCode()); //getter method
+        System.out.println("Status Line is "+response.getStatusLine());
+        System.out.println("Content Type is "+response.getContentType());
+        System.out.println("Time is "+response.getTime());
+        System.out.println("Headers are \n"+response.getHeaders());
+        System.out.println("Via is "+response.getHeader("Via"));
+
+
 
     }
 
